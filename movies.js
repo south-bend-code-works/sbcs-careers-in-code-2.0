85b261e115410c960679cffcd5370314e317401e
david_smith <david_smith@sweetwater.com>
2025-01-13 12:25:53 -0500

    dev Created `movies.js`.

---
+++ movies.js
@@ -0,0 +1,16 @@
+const movies = [
+  {
+    "id": 1,
+    "title": "Inception",
+    "genre": "Sci-Fi",
+    "release_year": 2010,
+    "poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+  },
+  {
+    "id": 2,
+    "title": "The Matrix",
+    "genre": "Action",
+    "release_year": 1999,
+    "poster": "https://m.media-amazon.com/images/M/MV5BN2NmN2VhMTQtMDNiOS00NDlhLTliMjgtODE2ZTY0ODQyNDRhXkEyXkFqcGc@._V1_SX300.jpg"
+  }
+];
