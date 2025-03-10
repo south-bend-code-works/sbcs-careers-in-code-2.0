b108ed827e149c7654a658b43c8b21a8ea3d3ed6
david_smith <david_smith@sweetwater.com>
2025-03-10 17:27:20 -0400

    js Created `final/stats.js`.

---
+++ final/stats.js
@@ -0,0 +1,252 @@
+const stats = [
+    {
+        "movie_id": 0,
+        "score": "74",
+        "box_office": "292587330"
+    },
+    {
+        "movie_id": 1,
+        "score": "73",
+        "box_office": "172076928"
+    },
+    {
+        "movie_id": 2,
+        "score": "97",
+        "box_office": "0"
+    },
+    {
+        "movie_id": 3,
+        "score": "100",
+        "box_office": "136381073"
+    },
+    {
+        "movie_id": 4,
+        "score": "90",
+        "box_office": "47834595"
+    },
+    {
+        "movie_id": 5,
+        "score": "84",
+        "box_office": "534987076"
+    },
+    {
+        "movie_id": 6,
+        "score": "97",
+        "box_office": "0"
+    },
+    {
+        "movie_id": 7,
+        "score": "95",
+        "box_office": "96898818"
+    },
+    {
+        "movie_id": 8,
+        "score": "94",
+        "box_office": "381878219"
+    },
+    {
+        "movie_id": 9,
+        "score": "95",
+        "box_office": "107928762"
+    },
+    {
+        "movie_id": 10,
+        "score": "90",
+        "box_office": "25100000"
+    },
+    {
+        "movie_id": 11,
+        "score": "67",
+        "box_office": "37030102"
+    },
+    {
+        "movie_id": 12,
+        "score": "92",
+        "box_office": "319372078"
+    },
+    {
+        "movie_id": 13,
+        "score": "87",
+        "box_office": "345518923"
+    },
+    {
+        "movie_id": 14,
+        "score": "84",
+        "box_office": "108981275"
+    },
+    {
+        "movie_id": 15,
+        "score": "92",
+        "box_office": "46909721"
+    },
+    {
+        "movie_id": 16,
+        "score": "98",
+        "box_office": "820278"
+    },
+    {
+        "movie_id": 17,
+        "score": "82",
+        "box_office": "292753960"
+    },
+    {
+        "movie_id": 18,
+        "score": "82",
+        "box_office": "330455270"
+    },
+    {
+        "movie_id": 19,
+        "score": "79",
+        "box_office": "7564459"
+    },
+    {
+        "movie_id": 20,
+        "score": "86",
+        "box_office": "130742922"
+    },
+    {
+        "movie_id": 21,
+        "score": "82",
+        "box_office": "5321508"
+    },
+    {
+        "movie_id": 22,
+        "score": "0",
+        "box_office": ""
+    },
+    {
+        "movie_id": 23,
+        "score": "96",
+        "box_office": "15205725"
+    },
+    {
+        "movie_id": 24,
+        "score": "62",
+        "box_office": "6719864"
+    },
+    {
+        "movie_id": 25,
+        "score": "76",
+        "box_office": "23341568"
+    },
+    {
+        "movie_id": 26,
+        "score": "64",
+        "box_office": "19501238"
+    },
+    {
+        "movie_id": 27,
+        "score": "74",
+        "box_office": "203227580"
+    },
+    {
+        "movie_id": 28,
+        "score": "97",
+        "box_office": "32000000"
+    },
+    {
+        "movie_id": 29,
+        "score": "61",
+        "box_office": "136801374"
+    },
+    {
+        "movie_id": 30,
+        "score": "100",
+        "box_office": "4219709"
+    },
+    {
+        "movie_id": 31,
+        "score": "99",
+        "box_office": "19181"
+    },
+    {
+        "movie_id": 32,
+        "score": "57",
+        "box_office": "10198820"
+    },
+    {
+        "movie_id": 33,
+        "score": "96",
+        "box_office": "163577"
+    },
+    {
+        "movie_id": 34,
+        "score": "86",
+        "box_office": "248159971"
+    },
+    {
+        "movie_id": 35,
+        "score": "100",
+        "box_office": "36764313"
+    },
+    {
+        "movie_id": 36,
+        "score": "85",
+        "box_office": "32590750"
+    },
+    {
+        "movie_id": 37,
+        "score": "85",
+        "box_office": "132399394"
+    },
+    {
+        "movie_id": 38,
+        "score": "89",
+        "box_office": "11286112"
+    },
+    {
+        "movie_id": 39,
+        "score": "91",
+        "box_office": "217049603"
+    },
+    {
+        "movie_id": 40,
+        "score": "58",
+        "box_office": "57563264"
+    },
+    {
+        "movie_id": 41,
+        "score": "94",
+        "box_office": "299645"
+    },
+    {
+        "movie_id": 42,
+        "score": "0",
+        "box_office": "0"
+    },
+    {
+        "movie_id": 43,
+        "score": "80",
+        "box_office": "12397210"
+    },
+    {
+        "movie_id": 44,
+        "score": "83",
+        "box_office": "25544867"
+    },
+    {
+        "movie_id": 45,
+        "score": "94",
+        "box_office": "96042913"
+    },
+    {
+        "movie_id": 46,
+        "score": "84",
+        "box_office": "38371200"
+    },
+    {
+        "movie_id": 47,
+        "score": "87",
+        "box_office": "214553307"
+    },
+    {
+        "movie_id": 48,
+        "score": "90",
+        "box_office": "0"
+    },
+    {
+        "movie_id": 49,
+        "score": "78",
+        "box_office": "448149584"
+    }
+];
